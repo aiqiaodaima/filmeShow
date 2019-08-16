7bed37e7b354d3110c8f3b1f481404632b4a138b
黎生 <async@yumail.cn>
2019-08-16 14:52:42 +0800

    修改

--- src/views/filmShow/add.vue
+++ src/views/filmShow/add.vue
@@ -3,8 +3,8 @@
     <div class="content-show">
       <ul class="out-carousel">
         <li class="big-carousel">
-          <div class="single-carousel" :class="classObject">
-            <swiper :options="swiperOption" v-if="planMovieList.length">
+          <div class="single-carousel" :class="classObject" v-if="planMovieList.length">
+            <swiper :options="swiperOption">
               <swiper-slide v-for="(item,index) in planMovieList" :key="index">
                 <div style="height:238px">
                   <img :src="item.moviePicUrl" alt="">
@@ -38,8 +38,11 @@
   </div>
 </template>
 <script>
-  import { swiper, swiperSlide} from 'vue-awesome-swiper'
-   
+  import {
+    swiper,
+    swiperSlide
+  } from 'vue-awesome-swiper'
+
   export default {
     components: {
       swiper,
@@ -69,13 +72,15 @@
           slidesPerView: 6,
           spaceBetween: 15,
           direction: 'horizontal',
-          // speed: 300,
+          speed: 300,
           // autoplay: { // 自动切换
           //   delay: 2500,
           //   disableOnInteraction: false
           // },
           loop: true,
-          // loopedSlides: 2,
+          loopedSlides: 2,
+          slidesPerView: '5',
+          loopedSlides: 5,
           pagination: {
             el: '.swiper-pagination',
             clickable: true
@@ -87,13 +92,13 @@
           grabCursor: true,
           centeredSlides: true,
           slidesPerView: 'auto',
-          coverflowEffect: {
-            rotate: 10,
-            stretch: -10,
-            depth: 10,
-            modifier: 2,
-            slideShadows: true
-          },
+          // coverflowEffect: {
+          //   rotate: 10,
+          //   stretch: -10,
+          //   depth: 10,
+          //   modifier: 2,
+          //   slideShadows: true
+          // },
           speed: 300,
           // autoplay: { // 自动切换
           //   delay: 1000,
@@ -195,10 +200,6 @@
     }
 
     .content-show {
-      margin: 2rem;
-      display: flex;
-      justify-content: space-between;
-
       .out-carousel {
         // display: flex;
         flex-direction: row;
@@ -207,40 +208,51 @@
 
         .big-carousel {
           width: 100%;
-          margin: 0 1em;
-
+          // margin: 1em;
           .single-carousel {
-            height: 700px;
             overflow: hidden;
-            border: 1px solid #333;
-            background-color: rgb(10, 30, 45);
-
+            height: 100vh;
             .swiper-slide {
               padding: 1em;
 
               .tip-white {
-                color: white;
-                line-height: 32px;
-                height: 32px;
+                width: 230px;
+                font-family: PingFangSC-Semibold;
+                font-size: 14px;
+                color: #FFFFFF;
                 text-align: center;
+                line-height: 57px;
+                overflow: hidden;
+                /*内容超出宽度时隐藏超出部分的内容 */
+                text-overflow: ellipsis;
+                /* 当对象内文本溢出时显示省略标记(...) ；需与overflow:hidden;一起使用。*/
+                white-space: nowrap;
+                /*不换行 */
               }
             }
 
             .swiperOption_v {
+              width: 230px;
+
               .swiper-slide {
                 width: 100%;
+
                 padding: 0;
 
                 // height: 40px;
                 // background-color: red;
                 .flim-Info {
+                  // height: 124px;
+                  height: 86px;
+                  margin-bottom: 20px;
                   border: 2px solid white;
                   position: relative;
-                  padding: 8px;
-                  border-radius: 5px;
+                  // padding: 8px;
+                  border-radius: 3px;
 
                   .flim-time {
-                    font-size: 16px;
+                    font-size: 28px;
+                    font-family: DINAlternate-Bold;
                     color: white;
                     padding: 0.5em;
                   }
@@ -252,16 +264,17 @@
 
                     .price-vip {
                       display: block;
-                      background: white;
                       color: #333;
-
+                      font-size: 12px;
+                      padding: 2px 4px 2px 8px;
+                      background-image: url("./filmShow.png")
                     }
 
                     .price-normal {
                       display: block;
                       color: white;
                       text-align: right;
-                      padding: 4px;
+                      padding: 2px 4px;
                     }
                   }
 
@@ -269,36 +282,54 @@
                     display: flex;
 
                     .type-language {
-                      background: yellow;
+                      background: #FDFF82;
+                      border-radius: 4px;
                       color: #000;
-                      padding: 2px;
+                      padding: 0 2px;
+                      height: 16px;
+                      font-size: 14px;
+                      line-height: 16px;
+                      // height: 24px;
                       white-space: nowrap;
-                      border-radius: 1px;
+                      // line-height: 24px;
+                      margin: 0 2px;
+                      // margin: 8px 0 8px 8px;
                     }
 
                     .type-type {
-                      padding: 2px;
-                      background: #999;
-                      color: white;
+                      padding: 0 2px;
+                      line-height: 16px;
+                      background: #181A21;
+                      // background: #3F0021;
+                      opacity: 0.8;
+                      border-radius: 4px;
+                      // color: white;
                       white-space: nowrap;
+                      height: 16px;
+                      line-height: 16px;
                     }
 
                     .type-position {
+                      font-family: PingFangSC-Semibold;
+                      font-size: 13px;
+                      color: #FFBAC9;
                       padding: 2px;
-                      // background: #999;
-                      color: white;
+                      color: #A9B1CE;
                       white-space: nowrap;
+                      margin-top: -3px;
                     }
 
                     .type-price {
+                      margin-top: -3px;
+                      font-family: DINAlternate-Bold;
+                      font-size: 12px;
+                      line-height: 24px;
+                      color: #FFBAC9;
                       font-size: 12px;
                       color: rgb(100, 106, 118);
-                      line-height: 16px;
+                      padding: 0px 0 0 4px;
                     }
                   }
-
-                  // width: 200px;
-                  // height: 100px;
                 }
               }
             }
